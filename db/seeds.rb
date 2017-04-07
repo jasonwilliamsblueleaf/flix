646d564b433ff2fbd36b2feace5dae9bb255ef48
Jason Williams <jason.williams@blueleaf.com>
2017-04-07 18:31:52 -0400

    updated db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -108,9 +108,21 @@ movie = Movie.find_by(title: 'Iron Man')
 movie.reviews.create!(name: "Roger Ebert", stars: 3, comment: "I laughed, I cried, I spilled my popcorn!")
 movie.reviews.create!(name: "Gene Siskel", stars: 5, comment: "I'm a better reviewer than he is.")
 movie.reviews.create!(name: "Peter Travers", stars: 4, comment: "It's been years since a movie superhero was this fierce and this funny.")
-
 movie = Movie.find_by(title: 'Superman')
 movie.reviews.create!(name: "Elvis Mitchell", stars: 5, comment: "It's a bird, it's a plane, it's a blockbuster!")
 
-names = ["Action", "Comedy", "Drama", "Romance", "Thriller", "Fantasy", "Documentary", "Adventure", "Animation", "Sci-Fi"]
-names.each { |name| Genre.create!(name: name)}
+movie = Movie.find_by(title: 'Iron Man')
+movie.fans << roger
+movie.fans << gene
+movie.fans << elvis
+
+Genre.create!(name: "Action")
+Genre.create!(name: "Comedy")
+Genre.create!(name: "Drama")
+Genre.create!(name: "Romance")
+Genre.create!(name: "Thriller")
+Genre.create!(name: "Fantasy")
+Genre.create!(name: "Documentary")
+Genre.create!(name: "Adventure")
+Genre.create!(name: "Animation")
+Genre.create!(name: "Sci-Fi")
